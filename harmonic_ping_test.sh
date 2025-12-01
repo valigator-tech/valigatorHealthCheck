4330077af91449d9f7664a53f114e39ecff54220
jasper9 <josh@jtri.com>
2025-12-01 16:34:27 -0700

    add harmonic endpoint check

---
+++ harmonic_ping_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Define hosts to ping
+HOSTS=(
+    "ams.auction.harmonic.gg"
+    "ewr.auction.harmonic.gg"
+)
+
+# Number of pings to send
+COUNT=5
+
+# Print header
+printf "%-45s %-8s %-8s %-8s %-8s\n" "Host" "Min" "Avg" "Max" "Loss%"
+printf "%s\n" "--------------------------------------------------------------------------------"
+
+# Loop through each host
+for host in "${HOSTS[@]}"; do
+    # Get ping statistics
+    stats=$(ping -c $COUNT $host | tail -1)
+    
+    # Extract ping times and packet loss
+    if [[ $stats =~ ([0-9.]+)%\ packet\ loss ]]; then
+        loss="${BASH_REMATCH[1]}"
+    else
+        loss="N/A"
+    fi
+    
+    if [[ $stats =~ min/avg/max.*\ =\ ([0-9.]+)/([0-9.]+)/([0-9.]+) ]]; then
+        min="${BASH_REMATCH[1]}"
+        avg="${BASH_REMATCH[2]}"
+        max="${BASH_REMATCH[3]}"
+    else
+        min="N/A"
+        avg="N/A"
+        max="N/A"
+    fi
+    
+    # Print results in a formatted way
+    printf "%-45s %-8s %-8s %-8s %-8s\n" "$host" "${min}ms" "${avg}ms" "${max}ms" "$loss%"
+done
